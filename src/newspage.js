35844352d383e82ccaf61f594bd7f4aa77a74fe6
LemonArt13 <trembov20@gmail.com>
2023-03-19 14:29:02 +0200

    newspage

--- src/newspage.js
+++ src/newspage.js
@@ -1,5 +1,6 @@
 import "./styles/newspage.scss";
 import "./styles/ad.scss";
 import "./styles/footer.scss";
+import "./styles/navbar.scss";
 
 fetch("https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts");

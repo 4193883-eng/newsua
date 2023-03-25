92393411c1f1c89ea9cb06bec145b973a50a6892
LemonArt13 <trembov20@gmail.com>
2023-03-25 13:36:17 +0200

    newspage

--- src/newspage.js
+++ src/newspage.js
@@ -2,5 +2,23 @@ import "./styles/newspage.scss";
 import "./styles/ad.scss";
 import "./styles/footer.scss";
 import "./styles/navbar.scss";
+import axios from "axios";
 
-fetch("https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts");
+axios
+  .get("https://news-ua-api.onrender.com/api/posts/640c5a76ca86c0d14fbf13fe")
+  .then((res) => {
+    const items = {
+      bgimg: document.querySelector(".prev"),
+      prevTitle: document.querySelector(".prev_title"),
+      author: document.querySelector("#author"),
+      views: document.querySelector(".views"),
+      body: document.querySelector(".text--info"),
+    };
+    const page = res.data;
+    items.bgimg.style.background = `url(${page.thumbnailUrl}) no-repeat center`;
+    items.prevTitle.innerHTML = page.title;
+    items.author.innerHTML = page.author;
+    items.views.innerHTML = `${page.views} views`;
+    items.body.innerHTML = page.body;
+    console.log(res.data.items);
+  });

4a1f3ad52d75987367de95e2b638ab3e7237547c
rostyslaveMuha <rostyslavmushynskyi@gmail.com>
2023-03-26 14:23:42 +0300

    Edit, delete, add modal

--- src/admin.js
+++ src/admin.js
@@ -1,11 +1,19 @@
+import axios from "axios";
+import "./styles/modal.scss";
 import "./styles/admin.scss";
 import tableRow from "./partials/table_row.hbs";
+import {
+  activeModal,
+  openModal,
+  closeModal,
+  fetchById,
+} from "./scripts/toggleModal";
 
-fetch("https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts")
-  .then((resp) => resp.json())
-  .then((news) => {
+function fetchGet() {
+  axios.get("https://news-ua-api.onrender.com/api/posts").then((res) => {
     const tbody = document.getElementById("tbody");
-    news.forEach((item) => {
+    tbody.innerHTML = "";
+    res.data.items.forEach((item) => {
       const trimmedItem = {
         ...item,
         title: item.title.slice(0, 50),
@@ -13,8 +21,124 @@ fetch("https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts")
         body: item.body.slice(0, 50),
       };
       const result = tableRow(trimmedItem);
-      console.log(item);
-      console.log(tableRow(item));
       tbody.insertAdjacentHTML("afterbegin", result);
     });
+    editBtns();
+    deleteRow();
   });
+}
+
+fetchGet();
+
+function editBtns() {
+  const editButtons = document.querySelectorAll(".editBtn");
+  const editModal = document.querySelector("#editModal");
+  editButtons.forEach((editBtn) => {
+    editBtn.addEventListener("click", (e) => {
+      const targetId = editBtn.dataset.id;
+      fetchById(targetId, editModal);
+    });
+  });
+  const editForm = editModal.querySelector("form");
+  editForm.addEventListener("click", () => {
+    const title = editModal.querySelector("#titleInput").value;
+    const body = editModal.querySelector("#bodyTextarea").value;
+    const url = editModal.querySelector("#urlInput").value;
+    const author = editModal.querySelector("#authorInput").value;
+    const targetId = editForm.dataset.id;
+    fetchPut(targetId, title, body, author, url);
+  });
+}
+
+window.addEventListener("load", (e) => {
+  editModal.classList.add("transition");
+});
+
+const addBtn = document.querySelector("#addbtn");
+const addModal = document.querySelector("#addModal");
+addBtn.addEventListener("click", () => {
+  openModal(addModal);
+
+  const submitBtn = addModal.querySelector("#addSubmitBtn");
+
+  addModal.addEventListener("click", (e) => {
+    const closeAddBtn = addModal.querySelector("#closeModalBtn");
+    if (
+      e.target === closeAddBtn ||
+      e.target === closeAddBtn.querySelector("img")
+    ) {
+      closeModal();
+    }
+  });
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = addModal.querySelector("#titleInput").value;
+    const body = addModal.querySelector("#bodyTextarea").value;
+    const url = addModal.querySelector("#urlInput").value;
+    const author = addModal.querySelector("#authorInput").value;
+    fetchPost(title, body, url, author);
+    closeModal();
+  });
+});
+
+// axios.get("https://news-ua-api.onrender.com/api/posts").then((res) => {
+//   console.log(res.data.items);
+// });
+function fetchPost(title, body, url, author) {
+  axios
+    .post("https://news-ua-api.onrender.com/api/posts", {
+      author: author,
+      title: title,
+      body: body,
+      thumbnailUrl: url,
+    })
+    .then(() => {
+      window.location.reload();
+    });
+}
+
+function findRowData(id) {
+  axios.get(`https://news-ua-api.onrender.com/api/posts/${id}`).then(() => {
+    fetchDel(id);
+  });
+}
+
+function fetchDel(id) {
+  axios.delete(`https://news-ua-api.onrender.com/api/posts/${id}`).then(() => {
+    window.location.reload();
+  });
+}
+
+function deleteRow() {
+  const deleteBtns = document.querySelectorAll(".deleteBtn");
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", (e) => {
+      const targetId = deleteBtn.dataset.id;
+      findRowData(targetId);
+    });
+  });
+}
+
+function fetchPut(id, title, body, author, thumbnailUrl) {
+  axios
+    .put(`https://news-ua-api.onrender.com/api/posts/${id}`, {
+      title,
+      body,
+      author,
+      thumbnailUrl,
+    })
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// function isUrl(url) {
+//   const changedUrl = url.slice(0, 4);
+//   if (changedUrl === "http") {
+//     return url;
+//   } else {
+//   }
+// }
--- src/scripts/toggleModal.js
+++ src/scripts/toggleModal.js
@@ -1,4 +1,7 @@
-const fetchUrl = 'https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts/';
+import axios from "axios";
+
+const fetchUrl =
+  "https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts/";
 
 export let activeModal = null;
 const body = document.body;
@@ -6,41 +9,50 @@ const body = document.body;
 // Functions
 export function openModal(modalRef) {
   activeModal = modalRef;
-  activeModal.classList.add('is-active');
-  body.classList.add('is-fixed');
+  activeModal.classList.add("is-active");
+  body.classList.add("is-fixed");
 }
 
 export function closeModal() {
   if (activeModal !== null) {
-    activeModal.classList.remove('is-active');
+    activeModal.classList.remove("is-active");
     activeModal = null;
-    body.classList.remove('is-fixed');
+    body.classList.remove("is-fixed");
   }
 }
 
 // Запит по айді
 export function fetchById(targetId, modalRef) {
-  fetch(`https://my-json-server.typicode.com/olehhapuk/news_ua_db/posts/${targetId}`)
+  axios
+    .get(`https://news-ua-api.onrender.com/api/posts/${targetId}`)
     .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
       // Відкриття модалки
       openModal(modalRef);
 
-      modalRef.querySelector('[name="title"]').value = data.title;
-      modalRef.querySelector('[name="body"]').value = data.body;
-      modalRef.querySelector('[name="thumbnailUrl"]').value = data.thumbnailUrl;
-      modalRef.querySelector('.form-modal__image').value = data.thumbnailUrl;
+      modalRef.querySelector('[name="title"]').value = res.data.title;
+      modalRef.querySelector('[name="body"]').value = res.data.body;
+      modalRef.querySelector('[name="thumbnailUrl"]').value =
+        res.data.thumbnailUrl;
+      modalRef.querySelector(".form-modal__image").src = res.data.thumbnailUrl;
+      modalRef.querySelector('[name="author"]').value = res.data.author;
+      modalRef.querySelector("#editModalForm").dataset.id = res.data.id;
     });
 
   // Закриття модалки
-  modalRef.closeEditModalBtn.addEventListener('click', () => {
-    closeModal();
+  modalRef.addEventListener("click", (e) => {
+    const closeModalBtn = modalRef.querySelector("#closeModalBtn");
+    if (
+      e.target === closeModalBtn ||
+      e.target === closeModalBtn.querySelector("img")
+    ) {
+      closeModal();
 
-    modalRef.querySelector('[name="title"]').value = '';
-    modalRef.querySelector('[name="body"]').value = '';
-    modalRef.querySelector('[name="thumbnailUrl"]').value = '';
-    modalRef.querySelector('.form-modal__image').value = '';
+      modalRef.querySelector('[name="title"]').value = "";
+      modalRef.querySelector('[name="body"]').value = "";
+      modalRef.querySelector('[name="thumbnailUrl"]').value = "";
+      modalRef.querySelector(".form-modal__image").src = "";
+      modalRef.querySelector('[name="author"]').value = "";
+      modalRef.querySelector("#editModalForm").dataset.id = "";
+    }
   });
 }
